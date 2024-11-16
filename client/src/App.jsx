442bb226231f0f7b97fb065bdcbded64bd1c3891
a-adityamagar <adityamagar677@gmail.com>
2024-11-16 22:55:16 +0545

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    </>
+    <BrowserRouter>
+      <Navbar/>
+    </BrowserRouter>
   )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        'primary-dark': '#1d4ed8',
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  }
 }
